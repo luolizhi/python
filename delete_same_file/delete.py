7c21ea3a31829e3c5fc5aef1e43bddf3840f1b7b
luolizhi <luolizhi1221@gmail.com>
2016-06-18 17:24:39 +0800

    delete文件可以清理电脑上面文件名含有(的图片

--- delete_same_file/delete.py
+++ delete_same_file/delete.py
@@ -6,6 +6,7 @@ __author__ = 'hadoop'
 __mtime__ = '2016/6/18'
 """
 
+# 程序先运行，打印信息正确之后，取消os.remove(path)的注释，真正的删除文件
 
 import os
 from time import clock as now
@@ -13,7 +14,7 @@ from time import clock as now
 def list_dir(rootDir):
     fileNum=0
     deleteNum =0
-    str = r'副本'  # 删除文件名中含有）的文件
+    str = u'('  # 删除文件名中含有(的文件，这个可以根据实际需要改成想要的名字，现在是根据文件名识别，高级算法是根据图片内容找相似
     dirs = []
     dirs.append(rootDir)
     for root in dirs:
@@ -24,17 +25,14 @@ def list_dir(rootDir):
             else:
                 fileNum += 1
                 filename = os.path.basename(path)
-                print filename.decode('gbk')
-                if str in filename.decode('gbk'):
+                # print filename.decode('gbk')              #解码成gbk可以打印出中文名字，否则乱码
+                # print(filename.decode('gbk').find(str))   #find返回的是找到字符串的位置,没找到返回-1
+                if filename.decode('gbk').find(str)!= -1:
                     deleteNum += 1
                     print '删除文件：',path.decode('gbk')
-                    os.remove(path)
-
+                    # os.remove(path)
     print '文件总个数为：', fileNum
     print '删除文件总个数为：',deleteNum
-
-
-
 
 if __name__=='__main__':
     rootDir = raw_input("输入需要处理文件的根目录：")
